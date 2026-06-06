cce733908ceb8576d15f2a0cbc0eb856e0f97e1f
Kichul Kang <kckang1103@gmail.com>
2026-06-05 21:34:33 -0700

    Initial commit with day1.py and day2.py

---
+++ day1.py
@@ -0,0 +1,36 @@
+import math
+
+haein1 = 3.14
+haein2 = 3
+haein3 = "cute"
+haein4 = (4 - 5) * (5 + 4) / 2
+
+# 해인4를 프린트 하는 함수
+# print((4 - 5) * (5 + 4) / 2)
+
+# exercise 1
+# Print your age to the console.
+print(25)
+
+# exercise 2
+# Calculate and print the number of days, weeks, and months in 27 years. Don’t worry about leap years!
+# x can be
+def get_days_weeks_months(years):
+    days = 365 * years
+    weeks = 52 * years
+    months = 12 * years
+    return days, weeks, months
+
+print('days:', get_days_weeks_months(27)[0])
+print('weeks:', get_days_weeks_months(27)[1])
+print("months:", get_days_weeks_months(27)[2])
+
+
+# exercise 3
+# Calculate and print the area of a circle with a radius of 5 units. You can be as accurate as you like with the value of pi.
+def get_area_of_circle(radius):
+    return math.pi * radius**2
+
+print(get_area_of_circle(5))
+
+haein
---
+++ day2.py
@@ -0,0 +1,36 @@
+# exercise 1:
+# Ask the user for their name and age, assign these values to two variables, and then print them.
+
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+
+print(name, age)
+
+# exercise 2:
+# Investigate what happens when you try to assign a value to a variable that you’ve already defined.
+# Try printing the variable before and after you reuse the name.
+
+# 12345678910
+
+haein = 25
+print(haein) #25
+haein = 26
+print(haein) #26
+
+# exercise 3:
+# Below you’ll find some code with a number of errors.
+# Try to go through the program line by line and fix the issues in the code.
+# I’d encourage you to try running the program while you’re working on it,
+# as reading the error messages is great practice for debugging your own programs.
+
+hourly_wage = input("Please enter your hourly wage: ")
+print("Hourly wage: ")
+print(hourly_wage)
+
+hours_worked = input("How many hours did you work this week? ")
+print("Hours worked: ")
+print(hours_worked)
+
+
+
+
